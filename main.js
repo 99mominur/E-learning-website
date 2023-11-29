3654b052abd755ddad5ed7c5f203723e872564aa
Mominur <99mominur@gmail.com>
2023-11-29 17:07:11 +0600

    fqa added

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 // navbar  bg change on scroll
 
 window.addEventListener("scroll", () => {
-  document.querySelector("nav").classList.toggle("window-scroll",window.scrollY>100);
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 100);
+});
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    if (faq.classList.contains("open")) {
+      faq.classList.toggle("open");
+      const icon = faq.querySelector(".faq__icon i");
+      icon.className = "fa-solid fa-plus";
+      return;
+    }
+    const open = faq.parentNode.querySelectorAll(".open");
+    open.forEach((opn) => {
+      opn.classList.toggle("open");
+      const icon = opn.querySelector(".faq__icon i");
+      icon.className = "fa-solid fa-plus";
+    });
+    faq.classList.toggle("open");
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus";
+    } else {
+      icon.className = "fa-solid fa-plus";
+    }
+  });
 });
